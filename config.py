6cfb2db3ead6a55e7079d9d7b8b149c46732053b
sambhavshah0425 <mamtashah5154@gmail.com>
2026-05-17 03:16:53 +0530

    Fixed MongoDB Atlas config

--- config.py
+++ config.py
@@ -3,8 +3,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Config:
-    MONGO_URI = os.getenv("MONGO_URI")
-    DB_NAME = os.getenv("DB_NAME")
 
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+class Config:
+
+    MONGO_URI = os.environ.get(
+        "MONGO_URI",
+        "mongodb://localhost:27017"
+    )
+
+    DB_NAME = os.environ.get(
+        "DB_NAME",
+        "ai_prompt_db"
+    )
+
+    OPENAI_API_KEY = os.environ.get(
+        "OPENAI_API_KEY",
+        "mock_key"
+    )
