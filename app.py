aee643c5aeb18434362912547a443a02d514bd7f
sambhavshah0425 <mamtashah5154@gmail.com>
2026-05-17 01:45:13 +0530

    Prepared for deployment

--- app.py
+++ app.py
@@ -31,7 +31,13 @@ def create_app():
         return jsonify({"error": "Bad request"}), 400
 
     return app
+import os
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=False, host='0.0.0.0', port=5000)
+
+    app.run(
+        debug=False,
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 5000))
+    )
